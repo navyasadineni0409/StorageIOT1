a267f2ca7333844b403af9f6d7ba54496c4faf86
navyasadineni0409 <navyasadineni0409@gmail.com>
2023-01-27 15:12:08 +0530

    updated index

--- StorageIOT1/Controllers/HomeController.cs
+++ StorageIOT1/Controllers/HomeController.cs
@@ -3,13 +3,13 @@
 namespace AzureIOT1.Controllers
 {
   [ApiController]
-    [Route("home")]
+    [Route("")]
     public class HomeController : ControllerBase
     {
         [HttpGet]
         public ActionResult Get()
         {
-            var path = "www/index.html";
+            var path = "wwwroot/index.html";
             StreamReader reader = new StreamReader(path);
             var fileBytes = System.IO.File.ReadAllBytes(path);
             FileContentResult file = File(fileBytes, "text/html");
